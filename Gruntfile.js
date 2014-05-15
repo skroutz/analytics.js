2d86bf227b03c6e7f1736b12432a91c9b958ff10
Bill Trikalinos <billtrik@skroutz.gr>
2014-05-15 18:21:02 +0300

    Add karma-fixture plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,8 @@ module.exports = function(grunt) {
       tests:{
         files: [
           'spec/**/*_spec.coffee',
+          'spec/**/*.html',
+          'spec/**/*.json',
         ],
         tasks: [
           'karma:unit:run',
