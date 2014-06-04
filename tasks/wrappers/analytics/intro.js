d73e8e3941747a4aed68ce4f311b93f908ccff69
Bill Trikalinos <billtrik@skroutz.gr>
2014-06-04 14:57:08 +0300

    Make the payload FIF ready

--- tasks/wrappers/analytics/intro.js
+++ tasks/wrappers/analytics/intro.js
@@ -1,2 +1,8 @@
 (function(global){
+  // Set global to always reference the outer context's window
+  // Applicable when the script runs inside a FIF
+  if(global.inDapIF){
+    global = global.top;
+  }
+
   global._saq = global._saq || []
