14daa40452dfc5fca9a587b571474f0bf40ec0f9
Kostas Diamantis <kosdiamantis@skroutz.gr>
2018-05-22 15:16:55 +0300

    Set global object to the parent of the current frame when inside a FIF
    This fixes a problem with Same-origin security policy when analytics
    is loaded inside another iframe.
    
    The error:
    DOMException: Blocked a frame with origin "http://www.<domain>.com" from accessing a cross-origin frame

--- tasks/wrappers/analytics/intro.js
+++ tasks/wrappers/analytics/intro.js
@@ -1,6 +1,6 @@
 (function(global){
-  // Set global to always reference the outer context's window
+  // Set global to always reference the parent window context
   // Applicable when the script runs inside a FIF
   if(global.inDapIF){
-    global = global.top;
+    global = global.parent;
   }
