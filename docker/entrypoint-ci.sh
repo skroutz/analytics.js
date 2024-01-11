a9a4d7e8e4824480b758a6de3071066016eff771
MrGoumX <goumaschristos@gmail.com>
2024-01-11 11:03:38 +0200

    Use buster based node image

--- docker/entrypoint-ci.sh
+++ docker/entrypoint-ci.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+# Phantomjs expects the location of OPENSSL_CONF in
+# a different system path. Since we upgraded to buster
+# the OPENSSL_CONF can be found under /etc/ssl.
+export OPENSSL_CONF=/etc/ssl
+
 # fetch assets
 yarn install
 
